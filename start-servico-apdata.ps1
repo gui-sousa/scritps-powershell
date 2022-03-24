7258eb2154975ed954735ef912c41ad0a5a1ab40
gui-sousa <guira_naruto@hotmail.com>
2022-03-24 16:34:09 -0300

    Ajuste da estrutura de repetição para Do-While

--- start-servico-apdata.ps1
+++ start-servico-apdata.ps1
@@ -7,8 +7,18 @@ $servico = @(
   "FakeService8"
 )
 
-while ((Get-Service -Name $servico[$i]).Status -ne "Runing") {
+$i = 0 
+do {
+
+  Start-Sleep -Seconds 10
+  Start-Service -Name $servico.Length
+  $i++
+
+} while ((Get-Service -Name $servico[$i]).Status -ne "Runing")
+
+
+<#while ((Get-Service -Name $servico[$i]).Status -ne "Runing") {
   Start-Sleep -Seconds 10
   Start-Service -Name $servico[$i]
   $i++
-}
+}#>
