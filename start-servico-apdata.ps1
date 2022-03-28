e6db82a6fd3282cfad5bb3813bf6e2c6c681f4a8
gui-sousa <guira_naruto@hotmail.com>
2022-03-28 17:02:50 -0300

    Controle de incremento no contador

--- start-servico-apdata.ps1
+++ start-servico-apdata.ps1
@@ -11,10 +11,15 @@ $i = 0
 do {
 
   Start-Sleep -Seconds 10
-  Start-Service -Name $servico.Length
-  $i++
-
-} while ((Get-Service -Name $servico[$i]).Status -ne "Runing")
+  Start-Service -Name $servico[$i]
+  
+  if ($i < $servico.length) {
+    $i++
+  } else {
+    break
+  }
+  
+} while ((Get-Service -Name $servico[$i]).Status -ne "Runing") 
 
 
 <#while ((Get-Service -Name $servico[$i]).Status -ne "Runing") {
