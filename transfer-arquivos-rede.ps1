f46b3f89c4a0202b7ceecf24fa498fade7bb7f47
gui-sousa <guira_naruto@hotmail.com>
2022-03-30 12:40:50 -0300

    Remoção do parametro COPYALL

--- transfer-arquivos-rede.ps1
+++ transfer-arquivos-rede.ps1
@@ -5,4 +5,4 @@ $PASTAdestino = "\\angola\kuduro\rede\arquivos"
 <#Copia todos os arquivos e pastas dentro do endereço.
 Faz 3 tentativas em caso de falha, 
 aguardando 5 segundos entre cada tentativa#>
-Robocopy.exe $PASTAorigem $PASTAdestino /e /r:3 /w:5 /COPYALL
+Robocopy.exe $PASTAorigem $PASTAdestino /e /r:3 /w:5 #<Apenas se tiver acesso ADM>/COPYALL
